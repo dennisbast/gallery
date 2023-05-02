526d95fcd66d581b9542bb55f87ca4dec8c46ff2
Dennis Bastkowski <d.bastkowski@googlemail.com>
2023-05-02 19:57:44 +0200

    made website responsive

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const ImageCard = ({ image }) => {
-  const tags = image.tags.split(',');
-
+  const tags = image.tags.split(",");
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    <div className="max-w-xs rounded shadow-lg">
       <img src={image.webformatURL} alt="" className="w-full" />
       <div className="px-6 py-4">
         <div className="font-bold text-blue-900 text-xl mb-2">
@@ -28,13 +27,17 @@ const ImageCard = ({ image }) => {
       </div>
       <div className="px-6 py-4">
         {tags.map((tag, id) => (
-            <span key={id} className="inline-block bg-gray-200 rounded-full px-3 
-        py-1 text-sm font-semibold text-gray-700 mr-2">
-        #{tag}</span>
+          <span
+            key={id}
+            className="inline-block bg-gray-200 rounded-full px-3 
+        py-1 text-sm font-semibold text-gray-700 mr-2"
+          >
+            #{tag}
+          </span>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ImageCard;
--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -1,30 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const ImageSearch = ({ searchText }) => {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
-  const onSubmit = (e) =>{
+  const onSubmit = (e) => {
     e.preventDefault();
 
     searchText(text);
-  }
+  };
 
   return (
     <div>
-        <div className='max-w-sm rounded overflow-hidden my-10 mx-auto'>
-            <form onSubmit={onSubmit} className='w-full max-w-sm'>
-                <div className='flex items-center border-b-2 border-blue-700 py-2'>
-                    <input onChange={e => setText(e.target.value)} className='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight
-                    focus:outline-none' type="text" placeholder='Search Image Term...' />
-                    <button className='flex-shrink-0 bg-blue-700 hover:bg-blue-900 border-blue-700 hover:border-blue-900
-                    text-sm border-4 text-white py-1 px-2 rounded' type="submit">
-                        Search
-                    </button>
-                </div>
-            </form>
-        </div>
+      <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
+        <form onSubmit={onSubmit} className="w-full max-w-sm">
+          <div className="flex items-center border-b-2 border-blue-700 py-2">
+            <input
+              onChange={(e) => setText(e.target.value)}
+              className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight
+                    focus:outline-none"
+              type="text"
+              placeholder="Search Image Term..."
+            />
+            <button
+              className="flex-shrink-0 bg-blue-700 hover:bg-blue-900 border-blue-700 hover:border-blue-900
+                    text-sm border-4 text-white py-1 px-2 rounded"
+              type="submit"
+            >
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default ImageSearch;
